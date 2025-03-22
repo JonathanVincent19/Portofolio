f22a36280144cc8380264f1e5d78a1db0f48d9c9
JonathanVincent19 <jthan730@gmail.com>
2025-03-22 12:55:35 +0700

    portofolio baru

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <style>
+        /* Sidebar Styling */
+        #sidebar {
+            height: 100vh;
+            width: 250px;
+            position: fixed;
+            top: 0;
+            left: 0;
+            background-color: #343a40;
+            padding-top: 20px;
+        }
+        #sidebar a {
+            padding: 10px 15px;
+            text-decoration: none;
+            font-size: 18px;
+            color: white;
+            display: block;
+        }
+        #sidebar a:hover {
+            background-color: #495057;
+        }
+
+        /* Main Content (agar tidak tertutup sidebar) */
+        #main-content {
+            margin-left: 260px; /* Sesuaikan dengan lebar sidebar */
+            padding: 20px;
+        }
+    </style>
+    </head>
+    <body>
+    <div class="container">
+    @include('layout.nav')
+    @include('layout.side')
+    @yield ('container')
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script
+  src="https://code.jquery.com/jquery-3.7.1.min.js"
+  integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+  crossorigin="anonymous"></script>
+
+    <script>
+        $(document).ready(function(){
+            console.log('ready');
+        function confirmDelete(id){
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if(result.isConfirmed) {
+            window.location.href="{{url('profile/delete/')}}/"+id;
+            }
+        });
+     }
+
+        $('.btn-delete').on('click', function(){
+            console.log('test');
+            var id = $(this).data('id');
+            confirmDelete(id);
+         });
+        });
+    </script>
+    </body>
+</html>
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.master')
+@section('container')
+<!DOCTYPE html>
+<div class="container">
+    <body>
+    <img src="{{ asset('Screenshot 2024-10-04 at 17.57.57.png') }}" class="img-fluid" width="1000">
+        <h1>Jonathan Vincent Timothy Bakara</h1>
+        <p>089637221403</p>
+        <p>Computer Science</p>
+        <p>Deskripsi Diri</p>
+    </body>
+<div>
+
+@endsection
